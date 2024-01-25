3ee8a11624636712ad0d0ffccc921c0ce7ed3d56
Gowtham <gowthamramar1372@gmail.com>
2024-01-25 21:29:53 +0530

    Host changes

--- tamproject/settings.py
+++ tamproject/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shopcart.up.railway.app"]
-CSRF_HOSTS = ['https://shopcart.up.railway.app']
+CSRF_HOSTS = ['shopcart.up.railway.app']
 
 
 # Application definition
